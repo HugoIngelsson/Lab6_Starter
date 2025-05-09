baa579c8f3da190caa1e815ce92fec9a321cd961
HugoIngelsson <cingelsson@ucsd.edu>
2025-05-09 13:10:11 -0700

    Completed expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,8 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	if (localStorage.getItem('recipes') == null) return [];
+	return JSON.parse(localStorage.getItem('recipes'));
 }
 
 /**
@@ -39,6 +41,13 @@ function addRecipesToDocument(recipes) {
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	let main = document.querySelector("main");
+	for (const recipe of recipes) {
+		let element = document.createElement("recipe-card");
+		element.data = recipe;
+
+		main.append(element);
+	}
 }
 
 /**
